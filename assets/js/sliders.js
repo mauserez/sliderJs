d96ed343f3b60135e261eabb74bb61485e28671e
HomeMac <mauserez@mail.ru>
2023-06-25 13:14:23 +0300

    union sliders

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,10 +1,7 @@
 const sliderJs = (options) => {
 	let sl = {};
 	const galleryPhoto = document.querySelector(".projects__gallery-photo");
-	const buttonsContainer = document.querySelector(
-		".projects__big-slider-buttons"
-	);
-
+	const galleryPhotoSm = document.querySelector(".projects__small-gallery");
 	const dotsContainer = document.querySelector(".projects__big-slider-dots");
 	const projectsButtons = document.querySelector(".projects__buttons");
 	const projectText = document.querySelector(".projects__text");
@@ -17,8 +14,8 @@ const sliderJs = (options) => {
 		".projects__project-repair-cost"
 	);
 
-	sl.prevBtn = document.querySelector(".bg-slider-prev");
-	sl.nextBtn = document.querySelector(".bg-slider-next");
+	sl.prevBtnBigSlider = document.querySelector(".bg-slider-prev");
+	sl.nextBtnBigSlider = document.querySelector(".bg-slider-next");
 	sl.options = options;
 	sl.currentProject = options[0];
 	sl.currentIndex = 0;
@@ -28,26 +25,6 @@ const sliderJs = (options) => {
 	sl.options.forEach((element) => {
 		sl.gallery.push(element.image);
 	});
-
-	sl.drawProjectsButtons = () => {
-		let projectsButtonsHtml = "";
-		sl.options.forEach((element, idx) => {
-			projectsButtonsHtml += `<div
-			class="projects__button
-			${idx === 0 ? "projects__button-active" : ""}
-			">
-				${element.projectId}
-			</div>`;
-			projectsButtons.innerHTML = projectsButtonsHtml;
-
-			sl.projectsButtons = document.querySelectorAll(".projects__button");
-			Array.from(sl.projectsButtons).forEach(function (el, idx) {
-				el.addEventListener("click", () => {
-					sl.setActiveProject(idx);
-				});
-			});
-		});
-	};
 
 	sl.setProjectOptions = () => {
 		projectText.innerHTML = sl.currentProject.projectOptions.description;
@@ -59,27 +36,7 @@ const sliderJs = (options) => {
 
 	sl.setGalleryImg = (index) => {
 		galleryPhoto.style.backgroundImage = `url(/assets/img/gallery/${sl.gallery[index]})`;
-	};
-
-	sl.drawDots = () => {
-		dotsContainer.innerHTML = "";
-		let dotsHtml = "";
-
-		sl.gallery.forEach((element, index) => {
-			let activeDot =
-				index === sl.currentIndex ? "projects__big-slider-dot-active" : "";
-
-			dotsHtml += `<div data-idx="${index}" class="projects__big-slider-dot ${activeDot}"></div>`;
-		});
-
-		dotsContainer.innerHTML = dotsHtml;
-
-		sl.dots = document.querySelectorAll(".projects__big-slider-dot");
-		Array.from(sl.dots).forEach(function (el, idx) {
-			el.addEventListener("click", () => {
-				sl.setActiveProject(idx);
-			});
-		});
+		galleryPhotoSm.style.backgroundImage = `url(/assets/img/gallery/${sl.gallery[index]})`;
 	};
 
 	sl.setActiveDot = (idx) => {
@@ -96,7 +53,7 @@ const sliderJs = (options) => {
 		sl.projectsButtons[idx].classList.add("projects__button-active");
 	};
 
-	sl.setActiveProject = (idx) => {
+	sl.setActiveProject = (idx = 0) => {
 		sl.currentProject = options[idx];
 		sl.setProjectOptions();
 		sl.setActiveProjectButton(idx);
@@ -121,44 +78,56 @@ const sliderJs = (options) => {
 		sl.setActiveProject(idx);
 	};
 
-	sl.prevBtn.addEventListener("click", () => {
+	sl.prevBtnBigSlider.addEventListener("click", () => {
 		sl.prev();
 	});
 
-	sl.nextBtn.addEventListener("click", () => {
+	sl.nextBtnBigSlider.addEventListener("click", () => {
 		sl.next();
 	});
 
-	sl.drawDots();
-	sl.drawProjectsButtons();
-	sl.setActiveProject(0);
+	sl.drawDots = () => {
+		dotsContainer.innerHTML = "";
+		let dotsHtml = "";
 
-	return sl;
-};
+		sl.gallery.forEach((element, index) => {
+			let activeDot =
+				index === sl.currentIndex ? "projects__big-slider-dot-active" : "";
 
-const smSliderJs = (options) => {
-	let sl = {};
-	const galleryPhoto = document.querySelector(".projects__small-gallery");
-	const projectText = document.querySelector(".projects__text");
-	const projectCity = document.querySelector(".projects__project-city");
-	const projectSize = document.querySelector(".projects__project-size");
-	const projectRepairTime = document.querySelector(
-		".projects__project-repair-time"
-	);
-	const projectRepaiCost = document.querySelector(
-		".projects__project-repair-cost"
-	);
+			dotsHtml += `<div data-idx="${index}" class="projects__big-slider-dot ${activeDot}"></div>`;
+		});
 
-	sl.options = options;
-	sl.currentProject = options[0];
-	sl.currentIndex = 0;
-	sl.dots = [];
-	sl.gallery = [];
-	sl.options.forEach((element) => {
-		sl.gallery.push(element.image);
-	});
+		dotsContainer.innerHTML = dotsHtml;
 
-	sl.drawButtons = () => {
+		sl.dots = document.querySelectorAll(".projects__big-slider-dot");
+		Array.from(sl.dots).forEach(function (el, idx) {
+			el.addEventListener("click", () => {
+				sl.setActiveProject(idx);
+			});
+		});
+	};
+
+	sl.drawProjectsButtons = () => {
+		let projectsButtonsHtml = "";
+		sl.options.forEach((element, idx) => {
+			projectsButtonsHtml += `<div
+			class="projects__button
+			${idx === 0 ? "projects__button-active" : ""}
+			">
+				${element.projectId}
+			</div>`;
+			projectsButtons.innerHTML = projectsButtonsHtml;
+
+			sl.projectsButtons = document.querySelectorAll(".projects__button");
+			Array.from(sl.projectsButtons).forEach(function (el, idx) {
+				el.addEventListener("click", () => {
+					sl.setActiveProject(idx);
+				});
+			});
+		});
+	};
+
+	sl.drawSmGalleryButtons = () => {
 		let hiddenButton = "";
 
 		if (sl.gallery.length === 1) {
@@ -170,7 +139,7 @@ const smSliderJs = (options) => {
 				<img class="slider-icon sm-slider-next next ${hiddenButton}" alt="Следующая картинка"
 				src="/assets/icons/small_slider/next.svg"/>`;
 
-		galleryPhoto.innerHTML = buttonsHtml;
+		galleryPhotoSm.innerHTML = buttonsHtml;
 		sl.buttons = document.querySelectorAll(".sm-slider-prev,.sm-slider-next");
 
 		Array.from(sl.buttons).forEach(function (el, idx) {
@@ -186,43 +155,10 @@ const smSliderJs = (options) => {
 		});
 	};
 
-	sl.setProjectOptions = () => {
-		projectText.innerHTML = sl.currentProject.projectOptions.description;
-		projectSize.innerHTML = sl.currentProject.projectOptions.size;
-		projectCity.innerHTML = sl.currentProject.projectOptions.city;
-		projectRepairTime.innerHTML = sl.currentProject.projectOptions.repairTime;
-		projectRepaiCost.innerHTML = sl.currentProject.projectOptions.repairCost;
-	};
-
-	sl.setGalleryImg = (index) => {
-		galleryPhoto.style.backgroundImage = `url(/assets/img/gallery/${sl.gallery[index]})`;
-	};
-
-	sl.setActiveProject = (idx) => {
-		sl.currentProject = options[idx];
-		sl.setProjectOptions();
-		sl.setGalleryImg(idx);
-		sl.currentIndex = idx;
-	};
-
-	sl.prev = () => {
-		let idx = sl.currentIndex - 1;
-		if (sl.currentIndex === 0) {
-			idx = sl.gallery.length - 1;
-		}
-		sl.setActiveProject(idx);
-	};
-
-	sl.next = () => {
-		let idx = sl.currentIndex + 1;
-		if (sl.currentIndex === sl.gallery.length - 1) {
-			idx = 0;
-		}
-		sl.setActiveProject(idx);
-	};
-
-	sl.drawButtons();
-	sl.setActiveProject(0);
+	sl.drawDots();
+	sl.drawSmGalleryButtons();
+	sl.drawProjectsButtons();
+	sl.setActiveProject();
 
 	return sl;
 };
